82430d55b0da5b0e414a6ee026479ea0ba51f7d3
ben_o <bentoki.1213@gmail.com>
2022-05-24 16:14:14 +0000

    weather　関連記述#

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/microsoft/vscode-remote-try-go
+module github.com/benono/weahther_line_bot_balk
 
 go 1.14
+
+require (
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible
+	github.com/microsoft/vscode-remote-try-go v0.0.0-20220517194155-6df7eedcd191
+)
--- server.go
+++ server.go
@@ -6,10 +6,14 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 
+	"work/weather"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/microsoft/vscode-remote-try-go/hello"
 )
 
@@ -18,8 +22,26 @@ func handle(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	portNumber := "9000"
-	http.HandleFunc("/", handle)
-	fmt.Println("Server listening on port ", portNumber)
-	http.ListenAndServe(":"+portNumber, nil)
+	// LINE Botクライアント生成する
+	// BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// weatherパッケージパッケージから天気情報の文字列をを取得する
+	result, err := weather.GetWeather()
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage(result)
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
